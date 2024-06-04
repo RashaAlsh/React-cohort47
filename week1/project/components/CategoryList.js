4ed1b8a03a032fc70b487d0ba8aad0da12860d83
rasha <rasha_alshraim@hotmail.com>
2024-06-04 13:39:47 +0200

    add assignment

---
+++ week1/project/components/CategoryList.js
@@ -0,0 +1,10 @@
+
+import { categories } from "../fake-data/all-categories";
+import Category from "./Category";
+
+export default function CategoryList({ setActiveCategory, activeCategory }) {
+  const listItems = categories.map((category, i) =>
+    <Category setActiveCategory={setActiveCategory} category={category} activeCategory={activeCategory} key={category + i}/> 
+  );
+  return <ul className={'category-list'}>{listItems}</ul>;
+}
